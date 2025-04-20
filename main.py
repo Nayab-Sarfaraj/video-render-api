5578226018869f3d95503249d96e5de7531e0c63
Nayab-Sarfaraj <nayabsarfaraj@gmail.com>
2025-04-20 13:11:40 +0530

    integrated firebase upload

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ import requests
 import os
 from PIL import Image
 from io import BytesIO
+import uuid
+from dotenv import load_dotenv,dotenv_values
+from fastapi import FastAPI, UploadFile, File
+import firebase_admin
+from firebase_admin import credentials, storage
 import schemas
+import os
 
-# image_paths = ["./public/img1.jpeg", "./public/img2.jpeg", './public/cat.jpeg']
+load_dotenv()
 app=FastAPI()
-OUTPUT_VIDEO = "rendered_story.mp4"
+print(os.getenv("firebase_json"))
+
+
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'bookify-8084d.appspot.com'
+})
+
+
 
 def download_file(url, filename):
     r = requests.get(url)
@@ -23,6 +38,11 @@ def download_and_resize_image(url):
     path = f"temp_{hash(url)}.jpg"
     img.save(path)
     return path
+
+
+
+
+
 
 @app.post("/render")
 def render_video(data:schemas.request):
@@ -41,7 +61,7 @@ def render_video(data:schemas.request):
                 ImageClip(path)
                 .with_duration(audio.duration / len(image_paths))
                 .resized(height=720)
-                .resized(lambda t: 1 + 0.04 * t)
+                .resized(lambda t: 1 + 0.05 * t)
                 .with_position("center")
             )
             clips.append(clip)
@@ -52,7 +72,16 @@ def render_video(data:schemas.request):
         OUTPUT_VIDEO = "output.mp4"
         video.write_videofile(OUTPUT_VIDEO, fps=24)
 
-        return {"status": "done"}
+        # ✅ Upload to Firebase Storage
+        bucket = storage.bucket()
+        blob = bucket.blob(f"{uuid.uuid4().hex}.mp4")
+        with open(OUTPUT_VIDEO, "rb") as video_file:
+            blob.upload_from_file(video_file, content_type="video/mp4")
+        blob.make_public()  # Optional, makes it publicly accessible
+
+        # 👇 Return the video URL
+        return {"status": "done", "video_url": blob.public_url}
+
 
     except Exception as e:
         return {"error": str(e)}
